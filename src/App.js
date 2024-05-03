54dd042da0fe0d7942350b3b1c87fed08e0f6e60
Weslen Lakins <weslen.lakins@outlook.com>
2024-05-03 00:03:58 -0400

    Update styling and functionality of quote box component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [quote, setQuote] = useState({ text: "", author: "" });
+
+	const fetchQuote = async () => {
+		const response = await fetch("https://api.quotable.io/random");
+		const data = await response.json();
+		setQuote({ text: data.content, author: data.author });
+	};
+
+	useEffect(() => {
+		fetchQuote();
+	}, []);
+
+	return (
+		<div id='quote-box' className='quote-box'>
+			<p id='text' className='quote-text'>{`"${quote.text}"`}</p>
+			<p id='author' className='quote-author'>{`- ${quote.author}`}</p>
+			<div className='buttons'>
+				<button id='new-quote' onClick={fetchQuote}>
+					New Quote
+				</button>
+				<a
+					id='tweet-quote'
+					className='tweet-button'
+					href={`https://twitter.com/intent/tweet?text="${encodeURIComponent(
+						quote.text
+					)}" - ${encodeURIComponent(quote.author)}`}
+					target='_blank'
+					rel='noopener noreferrer'>
+					Tweet
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
